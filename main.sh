d7cad4008f1c6088d87102213526e4903a146804
reno <62394004+thepolarbear0818@users.noreply.github.com>
2022-01-26 20:25:30 +0000

    it prints the numers but nothing else

--- main.sh
+++ main.sh
@@ -5,22 +5,18 @@ INPUT=student.csv
 AAPUT=class_registration.csv
 OLDIFS=$IFS
 IFS=','
-[ ! -f $INPUT ] && { echo "$INPUT file not found"; exit 99; }
-while read flname name 
+[ ! -f $INPUT && $AAPUT ] && { echo "$INPUT file not found"; exit 99; }
+while read flname name en_name 
 do
-	#echo "Student Numer : $flname"
-	#echo "Name : $name"
-	#echo "$flname, `Student Number `, `name` ,$name" >> log.csv
-  echo ''
-
-done 
+	echo "En_name : "
+done < $INPUT
 while read id class_id register_time is_paid external_id 
 do 
   echo "Student Number : $id"
   echo "Number of classes : $class_id "
   #echo ''
 
-done 
+done < $AAPUT
 IFS=$OLDIFS
 
 
