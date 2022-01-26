a8df1ec923eca689f0f3138d4cab1bc5f4523f64
reno <62394004+thepolarbear0818@users.noreply.github.com>
2022-01-26 20:53:33 +0000

    hehehehehehe

--- main.sh
+++ main.sh
@@ -3,17 +3,20 @@ read input
 echo input 
 INPUT=student.csv
 AAPUT=class_registration.csv
+
 OLDIFS=$IFS
 IFS=','
 [ ! -f $INPUT && $AAPUT ] && { echo "$INPUT file not found"; exit 99; }
 while read flname name en_name 
 do
-	echo "En_name : "
+	echo $en_name >> names.txt 
 done < $INPUT
 while read id class_id register_time is_paid external_id 
 do 
+  
   echo "Student Number : $id"
   echo "Number of classes : $class_id "
+
   #echo ''
 
 done < $AAPUT
