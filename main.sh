f3e87ba9b3546f57f16308259463320c59a5015b
reno <62394004+thepolarbear0818@users.noreply.github.com>
2022-01-26 20:01:06 +0000

    ahahahah

--- main.sh
+++ main.sh
@@ -1,17 +1,30 @@
-logsave a.txt ./main.sh 
+#logsave a.txt ./main.sh 
+read input 
+echo input 
 INPUT=student.csv
+AAPUT=class_registration.csv
 OLDIFS=$IFS
 IFS=','
 [ ! -f $INPUT ] && { echo "$INPUT file not found"; exit 99; }
 while read flname name 
 do
-	echo "Student Numer : $flname"
-	echo "Name : $name"
-	
-  
+	#echo "Student Numer : $flname"
+	#echo "Name : $name"
+	#echo "$flname, `Student Number `, `name` ,$name" >> log.csv
+  echo ''
 
-done < $INPUT
+done 
+while read id class_id register_time is_paid external_id 
+do 
+  echo "Student Number : $id"
+  echo "Number of classes : $class_id "
+  #echo ''
+
+done 
 IFS=$OLDIFS
 
-./notmain.sh 
+
+
+
+#./notmain.sh 
 
